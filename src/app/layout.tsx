6901555f578e198749461b15dc2e767c7d90c067
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2024-01-28 02:16:19 +0530

    feat: added copyright

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 /**
- * © Code-Theft_ 2024. All rights reserved.
+ * © IEEE CS Kerala Chapter 2024. All rights reserved.
  *
  * This code is the property of [Your Name] and is protected by copyright law.
  * Unauthorized use, reproduction, or distribution is strictly prohibited.
  *
+ * @author Janajith D
  */
 
 import React from "react";
--- src/common/components/Link.tsx
+++ src/common/components/Link.tsx
@@ -1,6 +1,7 @@
 /**
+ * © IEEE CS Kerala Chapter 2024. All rights reserved.
  *
- * This code is the property of Code-Theft_ and is protected by copyright law.
+ * This code is the property of [Your Name] and is protected by copyright law.
  * Unauthorized use, reproduction, or distribution is strictly prohibited.
  *
  * @author Janajith D
