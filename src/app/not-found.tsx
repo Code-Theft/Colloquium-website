7bbc75f4f7d098bcdd9751a43ed29d749d1e6307
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2024-01-28 10:26:12 +0530

    refactor: changes in copyright

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 /**
  * © IEEE CS Kerala Chapter 2024. All rights reserved.
  *
- * This code is the property of [Your Name] and is protected by copyright law.
+ * This code is the property of IEEE CS Kerala Chapter and is protected by copyright law.
  * Unauthorized use, reproduction, or distribution is strictly prohibited.
  *
  * @author Janajith D
