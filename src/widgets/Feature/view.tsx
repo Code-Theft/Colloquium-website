9f9f3ae21a11e077429ab9657d7636f15fe2e3ba
Janajith-D <144233877+Janajith-D@users.noreply.github.com>
2024-02-12 12:43:24 +0530

    feat: hide img on mobile

--- src/widgets/Feature/view.tsx
+++ src/widgets/Feature/view.tsx
@@ -73,7 +73,7 @@ export default function FeatureView() {
           <img
             src="/images/assets/venue.jpg"
             alt={imageAlts.VENUE}
-            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            className="hidden sm:block w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width={2432}
             height={1442}
           />
