043e8261b35c59c0f75a61750b61ec3a0122ced2
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-02-13 19:09:20 +0530

    fix:Speaker images added

--- src/common/icons/IconTime.tsx
+++ src/common/icons/IconTime.tsx
@@ -11,8 +11,8 @@ export default function IconTime() {
       className="absolute left-1 top-1 h-5 w-5 text-ColloquiumOrangeNormal"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
       />
     </svg>
