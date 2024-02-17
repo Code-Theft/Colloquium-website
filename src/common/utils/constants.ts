951c3d5c82e21eefa811b6815759eab0eb0033ec
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2024-02-17 20:34:14 +0530

    style: added space in about section

--- src/common/utils/constants.ts
+++ src/common/utils/constants.ts
@@ -32,7 +32,7 @@ export const aboutUs = [
     id: 1,
     title: "About COLLOQUIUM",
     desc: "COLLOQUIUM, hosted annually by the IEEE Computer Society Kerala Chapter (CSKS), is a premier networking and knowledge-sharing event. Designed for IEEE and non-IEEE professionals, COLLOQUIUM provides a unique platform for sharing insights, staying updated on industry trends, and fostering interactions among professionals from diverse technological domains.Each edition focuses on a specific theme, and COLLOQUIUM'24 will highlight the pivotal field of Cloud Computing. Join us at this vibrant event to delve into the latest advancements, connect with experts, and explore the future of technology. Don't miss the opportunity to be part of the dynamic discussions and collaborative sessions that define COLLOQUIUM!",
-    img: "/images/logos/colloquium/logoblack.svg",
+    img: "/images/logos/colloquium/black.png",
     reverse: false,
   },
   {
